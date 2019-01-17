aed4d6199346ca1cd648fde8fa39d8859f27054a
赵金浩 <2481406659@qq.com>
2019-01-17 11:15:11 +0800

    添加敏感字符系统

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
+char arr[2000];
+int index = 0;
+int main()
+{
+    printf("minganzifuguanlixitong\n");
 
-int main(){
+    while (1)
+    {
+        printf("1---tianjiaminganzifu\n");
+        printf("2---shanchuminganzifu\n");
+        printf("3---chakansuoyouminzifu\n");
+        printf("4---tihuanneirongzhongdeminganzifu\n");
+        printf("5---tuichuxitong\n");
+        
+        printf("qingxuanzexiangyinggongneng\n");
 
-    printf("hello world!");
+        int code;
+        scanf("%d",&code);
+
+        if (code == 1)
+        {
+            printf("qingshuruyaotianjiademinganzifu\n");
+            char m;
+            scanf("%c",&m);
+            scanf("%c",&m);
+            arr[index] = m;
+            index++;
+            printf("tianjiachenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&m);
+        }
+
+    }
+
+   
 
     return 0;
 
